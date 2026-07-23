1a0caab12ca6724d11c6b63b24d9a1f29db141a4
Rushikesh Thakre <rushikeshthakre12@gmail.com>
2026-07-24 00:49:48 +0530

    Hardcode Railway URL

--- inventory-ui/src/services/api.js
+++ inventory-ui/src/services/api.js
@@ -1,16 +1,14 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "https://inventory-management-system-production-9ad8.up.railway.app/api",
+  baseURL: "https://inventory-management-system-production-57ba.up.railway.app/api",
 });
 
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
-
   if (token) {
     config.headers.Authorization = `Bearer ${token}`;
   }
-
   return config;
 });
 
